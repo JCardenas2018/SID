688b9eb08bc8256c57aaadbcc49c613664794a44
JCardenas2018 <jesus.cardenas@vallegrande.edu.pe>
2020-12-08 21:03:28 -0500

    Zuul Server

--- src/test/java/za/co/superdev/api/SuperdevZuulApplicationTests.java
+++ src/test/java/za/co/superdev/api/SuperdevZuulApplicationTests.java
@@ -1,10 +1,10 @@
-package za.co.superdev.crm;
+package za.co.superdev.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SuperdevCrmCustomerApplicationTests {
+class SuperdevZuulApplicationTests {
 
 	@Test
 	void contextLoads() {
